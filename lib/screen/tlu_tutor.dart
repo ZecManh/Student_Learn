6b8b410d1b8815ebf2c9fbc1e61b5fdf4b9d5651
minhhieu <minghieuu@gmail.com>
2023-10-24 17:12:45 +0700

    theme setting

--- lib/screen/tlu_tutor.dart
+++ lib/screen/tlu_tutor.dart
@@ -1,5 +1,12 @@
 import 'package:flutter/material.dart';
 
+// final kColorScheme=ColorScheme.fromSeed(seedColor:Color.fromARGB(255, 255, 223, 223));
+final kColorScheme =
+    ColorScheme.fromSeed(seedColor: Color.fromARGB(255, 205, 92, 8));
+
+final kDarkColorScheme = ColorScheme.fromSeed(
+    seedColor: Color.fromARGB(255, 205, 92, 8), brightness: Brightness.dark);
+
 class TluTutor extends StatelessWidget {
   const TluTutor({super.key});
 
@@ -7,13 +14,54 @@ class TluTutor extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      theme: ThemeData(brightness:Brightness.light ),
-      darkTheme: ThemeData(brightness: Brightness.dark),
+      darkTheme: ThemeData.dark().copyWith(
+        useMaterial3: true,
+        colorScheme: kDarkColorScheme,
+        appBarTheme: AppBarTheme().copyWith(
+            backgroundColor: kDarkColorScheme.primaryContainer,
+            foregroundColor: kDarkColorScheme.primary),
+        elevatedButtonTheme: ElevatedButtonThemeData(
+          style: ElevatedButton.styleFrom(
+              padding: EdgeInsets.symmetric(horizontal: 10),
+              shape: RoundedRectangleBorder(),
+              textStyle: TextStyle(fontSize: 20)),
+        ),
+      ),
+      theme: ThemeData().copyWith(
+        brightness: Brightness.light,
+        useMaterial3: true,
+        colorScheme: kColorScheme,
+        appBarTheme: AppBarTheme().copyWith(
+            backgroundColor: kColorScheme.primaryContainer,
+            foregroundColor: kColorScheme.primary),
+        elevatedButtonTheme: ElevatedButtonThemeData(
+          style: ElevatedButton.styleFrom(
+              padding: EdgeInsets.symmetric(horizontal: 10),
+              shape: RoundedRectangleBorder(),
+              textStyle: TextStyle(fontSize: 20)),
+        ),
+      ),
       themeMode: ThemeMode.system,
       title: 'Tlu Tutor',
       home: Scaffold(
-        appBar: AppBar(title: Text('Tlu Tutor'),),
-        body: Text('Hello'),
+        appBar: AppBar(
+          title: Text('Tlu Tutor'),
+        ),
+        body: Column(
+          children: [
+            Text(
+              'Hello',
+            ),
+            ElevatedButton.icon(
+              style: ElevatedButton.styleFrom(
+                  shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(10))),
+              onPressed: () {},
+              icon: Icon(Icons.alarm),
+              label: Text('Alarm'),
+            )
+          ],
+        ),
       ),
     );
   }
