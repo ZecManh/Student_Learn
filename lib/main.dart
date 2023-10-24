333e26c32ac32bd33fbe4bf96a717dc349cdaeed
minhhieu <minghieuu@gmail.com>
2023-10-24 17:31:02 +0700

    lock orientation

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,13 @@
+import 'dart:developer';
+
 import 'package:datn/screen/tlu_tutor.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
-void main(){
-  runApp(const TluTutor());
+void main() {
+  WidgetsFlutterBinding.ensureInitialized();
+  SystemChrome.setPreferredOrientations([DeviceOrientation.portraitUp])
+      .then((value) {
+    runApp(const TluTutor());
+  });
 }
