9e1964027738eea2a2ee089d4358388e70b8ed56
namth <huynamnn1@gmail.com>
2023-11-27 20:37:16 +0700

    update face detection

--- lib/screen/face_detection/face_detection.dart
+++ lib/screen/face_detection/face_detection.dart
@@ -1,22 +1,71 @@
 import 'package:flutter/material.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_ml_vision/firebase_ml_vision.dart';
 
-class DashBoardFaceID extends StatefulWidget {
-  const DashBoardFaceID({super.key});
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
+  runApp(MyApp());
+}
 
+class MyApp extends StatelessWidget {
   @override
-  State<StatefulWidget> createState() {
-    return _DashBoardFaceIDState();
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: FaceDetectionScreen(),
+    );
   }
 }
 
-class _DashBoardFaceIDState extends State<DashBoardFaceID> {
-  String data = "";
+class FaceDetectionScreen extends StatefulWidget {
+  @override
+  _FaceDetectionScreenState createState() => _FaceDetectionScreenState();
+}
+
+class _FaceDetectionScreenState extends State<FaceDetectionScreen> {
+  FirebaseVisionImage _image;
+  FaceDetector _faceDetector;
+
+  @override
+  void initState() {
+    super.initState();
+    _faceDetector = FirebaseVision.instance.faceDetector(
+      FaceDetectorOptions(
+        mode: FaceDetectorMode.accurate,
+        enableContours: true,
+      ),
+    );
+  }
+
+  @override
+  void dispose() {
+    _faceDetector.close();
+    super.dispose();
+  }
+
+  Future<void> _detectFaces() async {
+    // Replace 'path/to/your/image.jpg' with the path to your image file
+    final imageFile = FirebaseVisionImage.fromFilePath('assets/path/to/your/image.jpg');
+    final faces = await _faceDetector.processImage(imageFile);
+
+    for (Face face in faces) {
+      print('Bounding box: ${face.boundingBox}');
+      // You can access other face information here, such as landmarks, head rotation, etc.
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
-    print('dash board qr rebuild');
-
-    return SafeArea(
-      child: const Scaffold(),
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Face Detection'),
+      ),
+      body: Center(
+        child: ElevatedButton(
+          onPressed: _detectFaces,
+          child: Text('Detect Faces'),
+        ),
+      ),
     );
   }
 }
