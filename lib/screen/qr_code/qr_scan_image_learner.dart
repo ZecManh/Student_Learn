4fdb188bf41367ce2ba8af2c093313c8e54c00af
ZecManh <manhstrong13@gmail.com>
2023-12-28 15:07:27 +0700

    ud QR

--- lib/screen/qr_code/qr_scan_image_learner.dart
+++ lib/screen/qr_code/qr_scan_image_learner.dart
@@ -1,7 +1,6 @@
 import 'dart:io';
 
 import 'package:datn/database/auth/firebase_auth_service.dart';
-import 'package:datn/screen/learner/learner_update_info.dart';
 import 'package:datn/screen/qr_code/info_learner.dart';
 import 'package:flutter/material.dart';
 import 'package:scan/scan.dart';
@@ -10,7 +9,8 @@ import 'package:provider/provider.dart';
 import 'package:datn/model/user/user.dart' as model_user;
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:datn/database/firestore/firestore_service.dart';
-
+import 'dart:convert';
+import 'package:datn/screen/learner/search_tutor/tutor_show_info.dart';
 class QrScanImgLearner extends StatefulWidget {
   @override
   _QrScanImgLearnerState createState() => _QrScanImgLearnerState();
@@ -27,6 +27,32 @@ class _QrScanImgLearnerState extends State<QrScanImgLearner> {
   @override
   Widget build(BuildContext context) {
     model_user.User user = Provider.of<model_user.User>(context);
+    FirebaseAuth auth = FirebaseAuth.instance;
+    FirestoreService firestoreService = FirestoreService();
+    void _initInfo(dynamic scanData) async {
+      var dataScan = jsonDecode(scanData);
+      if (dataScan['type'] == 'tutor') {
+        var userFetch = await firestoreService.getTutorById(dataScan['uid']);
+        if (userFetch != null) {
+          // print(userFetch);
+          // Navigator.push(
+          //     context,
+          //     MaterialPageRoute(
+          //         builder: (context) =>
+          //             TuTorShowInfo(tutor: userFetch)));
+
+          Navigator.push(
+            context,
+            MaterialPageRoute(builder: (context) {
+              return Provider.value(
+                  value: user, child: TuTorShowInfo(tutor: userFetch));
+            }),
+          );
+          return;
+        }
+
+      }
+    }
     return Scaffold(
       appBar: AppBar(
         title: const Text('Quét QR qua ảnh'),
@@ -47,13 +73,8 @@ class _QrScanImgLearnerState extends State<QrScanImgLearner> {
                         setState(() {
                           qrcode = str;
                           if (qrcode != null) {
-                            Navigator.push(
-                              context,
-                              MaterialPageRoute(builder: (context) {
-                                return Provider.value(
-                                    value: user, child: InfoLearner());
-                              }),
-                            );
+                            _initInfo(qrcode);
+
                           }
                         });
                       }
@@ -71,7 +92,7 @@ class _QrScanImgLearnerState extends State<QrScanImgLearner> {
               // ),
             ],
           ),
-          Text('${qrcode}'),
+          // Text('${qrcode}'),
         ],
       ),
     );
